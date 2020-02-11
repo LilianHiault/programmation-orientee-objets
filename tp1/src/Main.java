c4bd2a86746ca613d5ff6a3405b7cc91646c59a3
Lilian Hiault <lilian.hiault@laposte.net>
2020-02-11 09:18:46 +0100

    Fin tp1

--- tp1/src/Main.java
+++ tp1/src/Main.java
@@ -3,10 +3,24 @@ import personne.*;
 
 public class Main {
   public static void main(String[] args) {
-    Date maDate = new Date(23, 12, 2000);
-    System.out.println(maDate.afficheDate());
-    Date  date2 = new Date(04,02,2020);
+    Date naissBeren = new Date(23, 12, 432);
+    Date  naissLuthien = new Date(04,02,400);
 
-    System.out.println(maDate.compareTo(date2));
+    // System.out.println(maDate.afficheDate());
+    // System.out.println(maDate.compareTo(date2));
+
+    Personne beren = new Personne("Erchamion", "Beren", 'm', naissBeren);
+    Personne luthien = new Personne("Tinùviel", "Lùthien", 'f', naissLuthien);
+    Date berenLuthienMari = new Date(27, 07, 458);
+    beren.mariage(luthien, berenLuthienMari);
+
+    Date naissThingol = new Date(07, 12, 200);
+    Personne thingol = new Personne("Thingol", "Elu", 'm', naissThingol);
+    luthien.premParent(thingol);
+
+    Date naissLilian = new Date(23, 12, 2000);
+    Personne lilian = new Personne("Hiault", "Lilian", 'm', naissLilian);
+    lilian.premParent(thingol);
+    System.out.println(luthien.estFraternel(lilian));
   }
 }
--- tp1/src/Personne.java
+++ tp1/src/Personne.java
@@ -4,15 +4,18 @@ import date.*;
 public class Personne {
   private String nom;
   private String prenom;
+  private char sexe;
   private Date naissance;
-  private boolean marie;
+  private boolean estMarie;
   private Date mariage;
+  private Personne parent1;
+  private Personne parent2;
 
   // Setters
-  public void setNom(nvNom) {
+  public void setNom(String nvNom) {
     this.nom = nvNom;
   }
-  public void setPrenom(nvPrenom) {
+  public void setPrenom(String nvPrenom) {
     this.prenom = nvPrenom;
   }
   // Getters
@@ -22,7 +25,75 @@ public class Personne {
   public String getPrenom() {
     return this.prenom;
   }
+  public char getSexe() {
+    return this.sexe;
+  }
 
   // Méthodes
-  public void marriage(Date dateMarriage) {}
+  public Personne(String monNom, String monPrenom,char s, Date dateNaissance) {
+    if (s != 'm' & s != 'f') {
+      throw new IllegalArgumentException("Le sexe doit être 'm' ou 'f'.");
+    }
+    this.nom = monNom;
+    this.prenom = monPrenom;
+    this.sexe = s;
+    this.naissance = dateNaissance;
+    this.estMarie = false;
+  }
+
+  public void mariage(Personne autre, Date dateMariage) {
+    if ((this.naissance.compareTo(dateMariage) > 0) || (autre.naissance.compareTo(dateMariage) > 0)) {
+      throw new IllegalArgumentException("la naissance doit être antérieur à la date du mariage !");
+    } else {
+      this.estMarie = true;
+      this.mariage = dateMariage;
+
+      autre.estMarie  = true;
+      autre.mariage = dateMariage;
+    }
+  }
+
+  public void premParent(Personne p1) {
+    if (this.naissance.compareTo(p1.naissance) < 0) {
+      throw new IllegalArgumentException("La personne est née avant son parent !");
+    } else {
+      this.parent1 = p1;
+    }
+  }
+
+  public void deuxParent(Personne p2) {
+    if (this.naissance.compareTo(p2.naissance) < 0) {
+      throw new IllegalArgumentException("La personne est née avant son parent !");
+    } else {
+      this.parent2 = p2;
+    }
+  }
+
+  public boolean memePerso(Personne autre) {
+    if ((this.nom == autre.nom) && (this.prenom == autre.prenom) && (this.sexe == autre.sexe) && (this.naissance.compareTo(autre.naissance) == 0)) {
+      return true;
+    }
+    return false;
+  }
+
+  public boolean estFraternel(Personne autre) {
+    boolean estFrere = false;
+    if(this.parent1 != null && autre.parent1 != null) {
+      // System.out.println("Parents1 non nuls");
+      if(this.parent1.memePerso(autre.parent1)) {
+        // System.out.println("estFrere parent1");
+        estFrere = true;
+      }
+    } else if((this.parent2 != null) && (autre.parent2 != null)) {
+      // System.out.println("Parents2 non nuls");
+      if(this.parent2.memePerso(autre.parent2)) {
+        estFrere = true;
+        // System.out.println("estfrere parent2");
+      } else {
+        estFrere = false;
+        // System.out.println("Non frere");
+      }
+    }
+    return estFrere;
+  }
 }
