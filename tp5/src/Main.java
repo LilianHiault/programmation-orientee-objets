69b3044282875345ccc653384a6f954524d573d5
Lilian Hiault <lilian.hiault@laposte.net>
2020-03-17 12:22:19 +0100

    - ajouts mineurs Variable - suppr Affect.java

--- tp5/src/Main.java
+++ tp5/src/Main.java
@@ -9,12 +9,12 @@ import symbole.*;
 class Main {
   public static void main(String[] args) {
 
-    Constante n1 = new Constante(1); //1
-    Constante n2 = new Constante(2);//2
-    Somme plus = new Somme(n1, n2);//3
-    Difference moins = new Difference(n1, plus);//-2
-    Multiplication mult = new Multiplication(n2, plus);//6
-    Division div = new Division(mult, moins);//-3
+    Constante n1 = new Constante(1); // 1
+    Constante n2 = new Constante(2); // 2
+    Somme plus = new Somme(n1, n2); // 3
+    Difference moins = new Difference(n1, plus); // -2
+    Multiplication mult = new Multiplication(n2, plus); // 6
+    Division div = new Division(mult, moins); // -3
 
     Expression exp = new Expression(div);
 
--- tp5/src/Variable.java
+++ tp5/src/Variable.java
@@ -2,10 +2,19 @@ package variable;
 
 import noeud.*;
 
-public class Variable /*extends Noeud*/ {
+public class Variable extends Noeud { // Utiliser Map <String, Double> ?
     private String nom;
+    private double valeur;
 
     public double resoudre() {
-      return 0;
+      return this.valeur; // Attention null si valeur non affectée
+    }
+
+    public String toString() {
+      return this.nom;
+    }
+
+    public void affecter(double val) {
+      this.valeur = val;
     }
 }
