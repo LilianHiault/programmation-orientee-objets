217f8d7fcc2eb5fe3dd3134e839490059c054851
Lilian Hiault <34962237+LilianHiault@users.noreply.github.com>
2020-04-28 18:17:00 +0200

    Modif Fonction.java : ajout méthode eval()

--- tp5/src/Fonction.java
+++ tp5/src/Fonction.java
@@ -2,21 +2,24 @@ package symbole;
 
 import opeFormelles.*;
 import noeud.*;
+import affectation.*;
 
 public class Fonction extends OpeFormelles{
-  private String nom;
-  private Noeud expression;
+    private String nom;
+    private Noeud expression;
 
-  public Fonction(String nom, Noeud expr) {
-    this.nom = nom;
-    this.expression = expr;
-  }
+    public Fonction(String nom, Noeud expr) {
+        this.nom = nom;
+        this.expression = expr;
+    }
 
-  public String toString() {
-    return nom + "() <- " + expression.toString();
-  }
+    public String toString() {
+        return nom + "() <- " + expression.toString();
+    }
 
-  public double resoudre() {
-    return expression.resoudre();
-  }
+    public double resoudre() {
+        return expression.resoudre();
+    }
+
+    public void eval(Affectation aff) {}
 }
